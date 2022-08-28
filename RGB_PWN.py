8f4c0e36b7e31dbe79f97d732d9bd21986d476f0
sriram2k4 <sriramparanthaman72@gmail.com>
2022-08-28 15:07:34 +0530

    Added Smooth Effects

--- RGB_PWN.py
+++ RGB_PWN.py
@@ -11,19 +11,17 @@ GPIO.setup(R_channel,GPIO.OUT)
 GPIO.setup(G_channel,GPIO.OUT)
 GPIO.setup(B_channel,GPIO.OUT)
 
-r = GPIO.PWM(R_channel,60)
-g = GPIO.PWM(G_channel,60)
-b = GPIO.PWM(B_channel,60)
+r = GPIO.PWM(R_channel,1000)
+g = GPIO.PWM(G_channel,1000)
+b = GPIO.PWM(B_channel,1000)	
 
-# Reverse
-# r.start(90)
-# g.start(90)
-# b.start(100)
-
-# input("fsdf");
+FAST = 0.005
+SLOW = 0.05
 
 while True:
 	try:
+
+		#RED -> WHITE -> CYAN (Full Wave)
 		for i in range(255):
 			increasing = ((i/255)*100)
 			decresing = (((255-i)/255)*100)
@@ -34,29 +32,87 @@ while True:
 			g.start(value_g)
 			b.start(value_b)
 			print(value_r,value_g,value_b)
-			sleep(0.01)
-		for i in range(255):
+			sleep(SLOW)
+
+		#WHITE (Half Wave)
+		for i in range(0,127):
 			increasing = ((i/255)*100)
 			decresing = (((255-i)/255)*100)
-			value_r = decresing
-			value_g = increasing
-			value_b = decresing
-			r.start(value_r)
-			g.start(value_g)
-			b.start(value_b)
-			print(value_r,value_g,value_b)
-			sleep(0.01)
-		for i in range(255):
+			r.start(decresing)
+			g.start(increasing)
+			b.start(increasing)
+			print(increasing,decresing)
+			sleep(FAST)
+
+		#MAGENTA (Half Wave)
+		for i in range(127,255):
 			increasing = ((i/255)*100)
 			decresing = (((255-i)/255)*100)
-			value_r = decresing
-			value_g = decresing
-			value_b = increasing
-			r.start(value_r)
-			g.start(value_g)
-			b.start(value_b)
-			print(value_r,value_g,value_b)
-			sleep(0.01)
+			r.start(decresing)
+			g.start(increasing)
+			b.start(decresing)
+			print(increasing,decresing)
+			sleep(SLOW)
+
+		#WHITE (Half Wave)
+		for i in range(0,127):
+				increasing = ((i/255)*100)
+				decresing = (((255-i)/255)*100)
+				r.start(increasing)
+				g.start(decresing)
+				b.start(increasing)
+				print(increasing,decresing)
+				sleep(FAST)
+
+		#GREEN (Half Wave)
+		for i in range(127,255):
+			increasing = ((i/255)*100)
+			decresing = (((255-i)/255)*100)
+			r.start(increasing)
+			g.start(decresing)
+			b.start(increasing)
+			print(increasing,decresing)
+			sleep(SLOW)
+
+		#WHITE (Half Wave)
+		for i in range(0,127):
+			increasing = ((i/255)*100)
+			decresing = (((255-i)/255)*100)
+			r.start(decresing)
+			g.start(increasing)
+			b.start(decresing)
+			print(increasing,decresing)
+			sleep(FAST)
+
+		#YELLOW (Half Wave)
+		for i in range(127,255):
+			increasing = ((i/255)*100)
+			decresing = (((255-i)/255)*100)
+			r.start(decresing)
+			g.start(decresing)
+			b.start(increasing)
+			print(increasing,decresing,decresing)
+			sleep(SLOW)
+
+		#WHITE (Half Wave)
+		for i in range(0,127):
+			increasing = ((i/255)*100)
+			decresing = (((255-i)/255)*100)
+			r.start(increasing)
+			g.start(increasing)
+			b.start(decresing)
+			print(increasing,decresing)
+			sleep(FAST)
+
+		#WHITE FULL WAVE
+		for i in range(127,255):
+			increasing = ((i/255)*100)
+			decresing = (((255-i)/255)*100)
+			r.start(decresing)
+			g.start(increasing)
+			b.start(increasing)
+			print(increasing,decresing,decresing)
+			sleep(FAST)
 
 	except KeyboardInterrupt as e:
 		print("Recieved Ctrl+C Quiting\n")
